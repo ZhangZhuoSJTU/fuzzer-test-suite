baa6c8f646de36da5733f32f031891cf0738d15f
Zhuo Zhang <zhan3299@purdue.edu>
2020-09-25 02:49:37 +0000

    freetype2-2017

--- freetype2-2017/build.sh
+++ freetype2-2017/build.sh
@@ -10,23 +10,38 @@ build_lib() {
   (cd BUILD && ./autogen.sh && ./configure --disable-shared --with-harfbuzz=no --with-bzip2=no --with-png=no && make clean && make all -j $JOBS)
 }
 
-get_git_revision git://git.sv.nongnu.org/freetype/freetype2.git cd02d359a6d0455e9d16b87bf9665961c4699538 SRC
-build_lib
-build_fuzzer
+build_exe() {
+  if [[ ! -d seeds ]]; then
+    mkdir seeds
+    git clone https://github.com/unicode-org/text-rendering-tests.git TRT
+    # TRT/fonts is the full seed folder, but they're too big
+    cp TRT/fonts/TestKERNOne.otf seeds/
+    cp TRT/fonts/TestGLYFOne.ttf seeds/
+    rm -fr TRT
+  fi
 
-if [[ ! -d seeds ]]; then
-  mkdir seeds
-  git clone https://github.com/unicode-org/text-rendering-tests.git TRT
-  # TRT/fonts is the full seed folder, but they're too big
-  cp TRT/fonts/TestKERNOne.otf seeds/
-  cp TRT/fonts/TestGLYFOne.ttf seeds/
-  rm -fr TRT
-fi
+  cp BUILD/src/tools/ftfuzzer/ftfuzzer.cc target.cc
+  prepare_target || exit 2
 
-if [[ $FUZZING_ENGINE == "hooks" ]]; then
-  # Link ASan runtime so we can hook memcmp et al.
-  LIB_FUZZING_ENGINE="$LIB_FUZZING_ENGINE -fsanitize=address"
-fi
-set -x
-$CXX $CXXFLAGS -std=c++11 -I BUILD/include -I BUILD/ BUILD/src/tools/ftfuzzer/ftfuzzer.cc BUILD/objs/.libs/libfreetype.a  $LIB_FUZZING_ENGINE -larchive -lz -o $EXECUTABLE_NAME_BASE
+  $CXX $CXXFLAGS -std=c++11 -I BUILD/include -I BUILD/ target.cc BUILD/objs/.libs/libfreetype.a -larchive -lz -o $EXECUTABLE_NAME_BASE.$1
+}
 
+get_source() {
+  if [[ ! -d SRC ]]; then
+    get_git_revision git://git.sv.nongnu.org/freetype/freetype2.git cd02d359a6d0455e9d16b87bf9665961c4699538 SRC
+  fi
+}
+
+get_source || exit 1
+
+setup_normal || exit 1
+build_lib || exit 1
+build_exe "normal" || exit 1
+
+setup_afl_clang || exit 1
+build_lib || exit 1
+build_exe "afl.clang" || exit 1
+
+setup_afl || exit 1
+build_lib || exit 1
+build_exe "afl" || exit 1
