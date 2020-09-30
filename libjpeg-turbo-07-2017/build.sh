810ca363d4aaf4e780c2dbc08664a3f2edcf8108
Zhuo Zhang <zhan3299@purdue.edu>
2020-09-30 17:45:12 +0000

    libjpeg-turbo-07-2017

--- libjpeg-turbo-07-2017/build.sh
+++ libjpeg-turbo-07-2017/build.sh
@@ -10,13 +10,29 @@ build_lib() {
   (cd BUILD && autoreconf -fiv && ./configure --disable-shared && make -j $JOBS)
 }
 
-get_git_revision https://github.com/libjpeg-turbo/libjpeg-turbo.git b0971e47d76fdb81270e93bbf11ff5558073350d SRC
-build_lib
-build_fuzzer
+build_exe() {
+  cp $SCRIPT_DIR/libjpeg_turbo_fuzzer.cc target.cc
+  prepare_target || exit 2
 
-if [[ $FUZZING_ENGINE == "hooks" ]]; then
-  # Link ASan runtime so we can hook memcmp et al.
-  LIB_FUZZING_ENGINE="$LIB_FUZZING_ENGINE -fsanitize=address"
-fi
-set -x
-$CXX $CXXFLAGS -std=c++11 $SCRIPT_DIR/libjpeg_turbo_fuzzer.cc -I BUILD BUILD/.libs/libturbojpeg.a $LIB_FUZZING_ENGINE -o $EXECUTABLE_NAME_BASE
+  $CXX $CXXFLAGS -std=c++11 -I BUILD target.cc BUILD/.libs/libturbojpeg.a -o $EXECUTABLE_NAME_BASE.$1
+}
+
+get_source() {
+  if [[ ! -d SRC ]]; then
+    get_git_revision https://github.com/libjpeg-turbo/libjpeg-turbo.git b0971e47d76fdb81270e93bbf11ff5558073350d SRC
+  fi
+}
+
+get_source || exit 1
+
+setup_normal || exit 1
+build_lib || exit 1
+build_exe "normal" || exit 1
+
+setup_afl_clang || exit 1
+build_lib || exit 1
+build_exe "afl.clang" || exit 1
+
+setup_afl || exit 1
+build_lib || exit 1
+build_exe "afl" || exit 1
