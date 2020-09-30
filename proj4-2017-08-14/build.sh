2f499a3a4ccf1dbaa12edbebabc52c369517b4d5
Zhuo Zhang <zhan3299@purdue.edu>
2020-09-30 20:12:49 +0000

    proj4-2017-08-14

--- proj4-2017-08-14/build.sh
+++ proj4-2017-08-14/build.sh
@@ -10,17 +10,34 @@ build_lib() {
   (cd BUILD && ./autogen.sh &&  ./configure --disable-shared &&  make clean  && make -j $JOBS )
 }
 
-get_git_revision https://github.com/OSGeo/proj.4.git d00501750b210a73f9fb107ac97a683d4e3d8e7a SRC
-build_lib
-build_fuzzer
+build_exe() {
+  if [[ ! -d seeds ]]; then
+    mkdir seeds
+    cp BUILD/nad/* seeds
+  fi
 
-if [[ ! -d seeds ]]; then
-  mkdir seeds
-  cp BUILD/nad/* seeds
-fi
+  cp BUILD/test/fuzzers/standard_fuzzer.cpp target.cc
+  prepare_target || exit 2
 
-if [[ $FUZZING_ENGINE == "hooks" ]]; then
-  # Link ASan runtime so we can hook memcmp et al.
-  LIB_FUZZING_ENGINE="$LIB_FUZZING_ENGINE -fsanitize=address"
-fi
-$CXX $CXXFLAGS -std=c++11 -I BUILD/src BUILD/test/fuzzers/standard_fuzzer.cpp BUILD/src/.libs/libproj.a $LIB_FUZZING_ENGINE -o $EXECUTABLE_NAME_BASE -lpthread
+  $CXX $CXXFLAGS -std=c++11 -I BUILD/src target.cc BUILD/src/.libs/libproj.a -o $EXECUTABLE_NAME_BASE.$1 -lpthread
+}
+
+get_source() {
+  if [[ ! -d SRC ]]; then
+    get_git_revision https://github.com/OSGeo/proj.4.git d00501750b210a73f9fb107ac97a683d4e3d8e7a SRC
+  fi
+}
+
+get_source || exit 1
+
+setup_normal || exit 1
+build_lib || exit 1
+build_exe "normal" || exit 1
+
+setup_afl || exit 1
+build_lib || exit 1
+build_exe "afl" || exit 1
+
+setup_afl_clang || exit 1
+build_lib || exit 1
+build_exe "afl.clang" || exit 1
