07423db005e818f58e2049266bc95f0d5c2432c9
Zhuo Zhang <zhan3299@purdue.edu>
2020-09-30 20:10:19 +0000

    pcre2-10.00

--- pcre2-10.00/build.sh
+++ pcre2-10.00/build.sh
@@ -14,13 +14,28 @@ build_lib() {
   )
 }
 
-get_svn_revision svn://vcs.exim.org/pcre2/code/trunk 183 SRC
-build_lib
-build_fuzzer
+build_exe() {
+  prepare_target || exit 2
 
-if [[ $FUZZING_ENGINE == "hooks" ]]; then
-  # Link ASan runtime so we can hook memcmp et al.
-  LIB_FUZZING_ENGINE="$LIB_FUZZING_ENGINE -fsanitize=address"
-fi
-set -x
-$CXX $CXXFLAGS $SCRIPT_DIR/target.cc -I BUILD/src -Wl,--whole-archive BUILD/.libs/*.a -Wl,-no-whole-archive $LIB_FUZZING_ENGINE -o $EXECUTABLE_NAME_BASE
+  $CXX $CXXFLAGS target.cc -I BUILD/src -Wl,--whole-archive BUILD/.libs/*.a -Wl,-no-whole-archive -o $EXECUTABLE_NAME_BASE.$1
+}
+
+get_source() {
+  if [[ ! -d SRC ]]; then
+    get_svn_revision svn://vcs.exim.org/pcre2/code/trunk 183 SRC
+  fi
+}
+
+get_source || exit 1
+
+setup_normal || exit 1
+build_lib || exit 1
+build_exe "normal" || exit 1
+
+setup_afl || exit 1
+build_lib || exit 1
+build_exe "afl" || exit 1
+
+setup_afl_clang || exit 1
+build_lib || exit 1
+build_exe "afl.clang" || exit 1
